9eb009e68ebaa31dd788210f101cb4b0b28ebbb3
Adarsh Kumar <adarshkumar9820@gmail.com>
2020-06-27 13:38:54 +0800

    unit test

--- command/types_test.go
+++ command/types_test.go
@@ -19,5 +19,9 @@ func TestHLSTranscoder_Build(t *testing.T) {
 	}
 
 	fmt.Println(cmd)
+	expected := "ffmpeg  -y -i input.mp4 -hide_banner -vf scale=640:360 -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename ./640_360_800_%03d.ts ./640_360_800.m3u8 -master_pl_name master.m3u8"
+	if cmd != expected {
+		t.Errorf("expected : %s, got : %s", expected, cmd)
+	}
 
 }
