3c6d23d05df008dcd4a98f17723af17658901c50
Adarsh Kumar <adarshkumar9820@gmail.com>
2020-07-02 12:02:05 +0800

    unit test

--- command/types.go
+++ command/types.go
@@ -55,7 +55,6 @@ func (b *HLSStreamBuilder) MasterFileName(masterFileName string) *HLSStreamBuild
 	return b
 }
 
-// -map 0:v -map 0:a -map 0:v -map 0:a -f hls -var_stream_map "v:0,a:0 v:1,a:1"
 func (b *HLSStreamBuilder) Build() (string, error) {
 	videoFilters := ""
 	for _, v := range b.hLSStream.videoFilters {
@@ -65,13 +64,5 @@ func (b *HLSStreamBuilder) Build() (string, error) {
 			Separator + fmt.Sprintf("%s/%s.m3u8", b.hLSStream.outputDirectoryPath, dimension)
 	}
 	b.hLSStream.command = b.hLSStream.command + Separator + videoFilters
-
-	//segmentPrefix := filepath.Join(b.hLSStream.outputDirectoryPath, "v%v/fileSequence%d.ts")
-	//segmentMasterPrefix := filepath.Join(b.hLSStream.outputDirectoryPath, "v%v/prog_index.m3u8")
-	//b.hLSStream.command = b.hLSStream.command + Separator + fmt.Sprintf("-hls_segment_filename %s", fmt.Sprintf("\"%s\"", segmentPrefix))
-	//b.hLSStream.command = b.hLSStream.command + Separator + segmentMasterPrefix
-
-	//b.hLSStream.command = b.hLSStream.command + Separator + fmt.Sprintf("-hls_flags single_file")
-
 	return b.hLSStream.command, nil
 }
